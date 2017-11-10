513ec75030d7e96eaf5743b2429ad6a1dbefae1c
karak <karak97@gmail.com>
2017-11-11 00:36:13 +0900

    Separated jest config from package.json.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "<rootDir>/node_modules/ts-jest/preprocessor.js"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$",
+  "testPathIgnorePatterns": [
+    "lib",
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "json"
+  ]
+}
